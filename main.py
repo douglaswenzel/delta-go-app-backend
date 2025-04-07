9a996e19890322d80f707fa5b1e7daaa1ca5ee94
douglaswenzel <douglas.wenzel@hotmail.com>
2025-04-07 09:06:17 -0300

    v2.0006.006 Definida conexão com banco de dados

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     filename='sistema_acesso.log'
 )
+
+class DatabaseManager:
+    def __init__(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host='162.241.2.230',
+                database='dougl947_DeltaGo',
+                user='dougl947_user',
+                password=''
+            )
+            logging.info("Conexão com o MySQL estabelecida com sucesso")
+        except Error as e:
+            logging.error(f"Erro ao conectar ao MySQL: {e}")
+            raise
